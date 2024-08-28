7e9c75d29db8d38d4b887e8394bbf9977b9db1f5
Barton-Loutit <94425592+Barton-Loutit@users.noreply.github.com>
2024-08-27 22:00:55 -0600

    Added enemy collision (hit and death), VFX
    Also added note to enemy manager script for score, session, and player stat management.
    
    Kept debug logging for enemy hit and death for future testing.
    
    Hit FX needs to be modified, currently particle effect mimics death fx.

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] GameObject enemyHitVFX;
+    [SerializeField] GameObject enemyDeathVFX;
+    [SerializeField] int enemyHitScoreValue = 0;
+    [SerializeField] int enemyDeathScoreValue = 0;
+    [SerializeField] int enemyHitPoints = 3;
+
+    int tempScoreTracker = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Process enemy hit on particle collision iff collision source is a "Weapon"
+    //Other particles, such as other enemy missiles, may trigger this if not
+    //scoped to playerweapon tag
+    void OnParticleCollision(GameObject other)
+    {
+        if(other.tag == "PlayerWeapon")
+        {
+            HandleEnemyHit();
+        }
+        
+    }
+
+    void HandleEnemyHit()
+    {
+        enemyHitPoints--;
+        if(enemyHitPoints <= 0)
+        {
+            HandleEnemyDeath();
+            Debug.Log("Enemy neutralized");
+        }
+        else
+        {
+            Debug.Log("Enemy hit");
+            /* Adding this comment block to keep track of a design decision
+             * To-Do: Add score on hit to player stats script/score manager or something
+             * Note-to-self: (a GameSessionManager could implement some functinoality related to score, bombs, tokens, continue state, etc.)(?)
+             * PlayerManager: Bombs, Health, 
+             * ScoreManager: Score, Score retention
+             * Game Session Manager: tokens, scene management, continue state
+             * Canvas/UI Manager: to display data from ScoreManager, GameSessionManager, PlayerManager
+             *      First To-Do:: Handle score as part of a scoreManager
+             *      Second To-Do: 
+            */
+            TempIncreaseScore(enemyHitScoreValue);
+            PlayEnemyVFX(enemyHitVFX);
+
+            //To-Do: Add SFX for enemy hit (Note-to-self: use serialized field & setup an audio manager singleton)
+        }
+    }
+    void HandleEnemyDeath()
+    {
+        PlayEnemyVFX(enemyDeathVFX);
+        TempIncreaseScore(enemyDeathScoreValue);
+        DestroyEnemyObject();
+    }
+
+    void DestroyEnemyObject()
+    {
+        if (!this.gameObject.IsDestroyed())
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //Note: Currently VFX objects clutter the hierarchy, and gameobjects are not destroyed after they're done playing
+    //may want to consider adding VFX destruction script.
+    void PlayEnemyVFX(GameObject enemyVFXToPlay)
+    {
+        GameObject vfx = Instantiate(enemyVFXToPlay, transform.position, Quaternion.identity);
+    }
+
+    //Creating this method definition to play SFX in the future.
+    void PlayEnemySFX(GameObject enemySFXToPlay)
+    {
+        //GameObject vfx = Instantiate(enemyVFXToPlay, transform.position, Quaternion.identity);
+        //This will probably need to be done differently
+    }
+
+    void TempIncreaseScore(int scoreToAdd)
+    {
+        tempScoreTracker += scoreToAdd;
+    }
+
+    
+}
